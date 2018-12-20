9631fdbb190cf275c8861c24af80ccd4f411cc60
MrHidalgo <Luckyhidalgo@gmail.com>
2018-12-20 09:19:37 +0200

    Search page - create results sections - w\ adaptive.

--- src/js/_lib/sectionBGResize.js
+++ src/js/_lib/sectionBGResize.js
@@ -1,14 +1,16 @@
 const initSectionBGResize = (parentNodeName) => {
-  const sectionResize = $('[section-resize-js]');
+  if($('[section-resize-js]').length) {
+    const sectionResize = $('[section-resize-js]');
 
-  const sectionRight = $('[section-right-js]'),
-    widthElem = sectionRight[0].getBoundingClientRect().left,
-    leftOffset = sectionResize.closest(parentNodeName)[0].getBoundingClientRect().left;
+    const sectionRight = $('[section-right-js]'),
+      widthElem = sectionRight[0].getBoundingClientRect().left,
+      leftOffset = sectionResize.closest(parentNodeName)[0].getBoundingClientRect().left;
 
-  if($(window).width() > 991) {
-    sectionResize.css({
-      'left' : -(leftOffset),
-      'width' : widthElem,
-    });
+    if($(window).width() > 991) {
+      sectionResize.css({
+        'left' : -(leftOffset),
+        'width' : widthElem,
+      });
+    }
   }
 };
