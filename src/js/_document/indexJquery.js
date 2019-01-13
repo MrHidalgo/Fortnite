5a2264498f9ff9fe7b364edac7c0f2599fd68bc6
MrHidalgo <Luckyhidalgo@gmail.com>
2019-01-13 23:42:54 +0200

    Edits - filter select.

--- src/js/_document/indexJquery.js
+++ src/js/_document/indexJquery.js
@@ -183,67 +183,111 @@ $(document).ready((ev) => {
       graph1 = document.getElementById("graph-1"),
       graph2 = document.getElementById("graph-2");
 
-    new Chart(graph0, {
-      type: "doughnut",
-      data: {
-        labels: [
-          'Outfits',
-          'Gliders',
-          'Pickaxes',
-          'Emotes'
-        ],
-        datasets: [{
-          data: [4,2,3,2],
-          borderWidth: 2,
-          backgroundColor: [
-            '#e98849',
-            '#c361f3',
-            '#3b9be4',
-            '#85e430'
-          ]
-        }]
+    if($('#graph-0').length > 0) {
+      new Chart(graph0, {
+        type: "doughnut",
+        data: {
+          labels: [
+            'Outfits',
+            'Gliders',
+            'Pickaxes',
+            'Emotes'
+          ],
+          datasets: [{
+            data: [4,2,3,2],
+            borderWidth: 2,
+            backgroundColor: [
+              '#e98849',
+              '#c361f3',
+              '#3b9be4',
+              '#85e430'
+            ]
+          }]
+        }
+      });
+
+      new Chart(graph1, {
+        type: "doughnut",
+        data: {
+          labels: [
+            'Daily',
+            'Featured'
+          ],
+          datasets: [{
+            data: [5,6],
+            borderWidth: 2,
+            backgroundColor: [
+              '#a8c7bb',
+              '#ffa69e',
+            ]
+          }]
+        }
+      });
+
+      new Chart(graph2, {
+        type: "doughnut",
+        data: {
+          labels: [
+            'Legendary',
+            'Rare',
+            'Epic',
+            'Uncommon'
+          ],
+          datasets: [{
+            data: [2,1,6,2],
+            borderWidth: 2,
+            backgroundColor: [
+              '#800020',
+              '#d8beca',
+              '#ff8d00',
+              '#b2edc9'
+            ]
+          }]
+        }
+      });
+    }
+  };
+
+
+  const initFilterRadioBtn = () => {
+    $(window).on('load', () => {
+      const checkboxBtn = $('input[type="checkbox"]');
+
+      for(let el of checkboxBtn) {
+        if($(el).is(':disabled')) {
+          $(el).closest('.c-form__field').addClass('is-disabled');
+        }
       }
     });
 
-    new Chart(graph1, {
-      type: "doughnut",
-      data: {
-        labels: [
-          'Daily',
-          'Featured'
-        ],
-        datasets: [{
-          data: [5,6],
-          borderWidth: 2,
-          backgroundColor: [
-            '#a8c7bb',
-            '#ffa69e',
-          ]
-        }]
-      }
-    });
+    $('input[type="checkbox"]').on('change', (ev) => {
+      const elem = $(ev.currentTarget),
+        elemParent = elem.closest('.filter__category-main'),
+        elemParentID = $(elemParent).data('checkboxhead');
 
-    new Chart(graph2, {
-      type: "doughnut",
-      data: {
-        labels: [
-          'Legendary',
-          'Rare',
-          'Epic',
-          'Uncommon'
-        ],
-        datasets: [{
-          data: [2,1,6,2],
-          borderWidth: 2,
-          backgroundColor: [
-            '#800020',
-            '#d8beca',
-            '#ff8d00',
-            '#b2edc9'
-          ]
-        }]
+      const checkboxParent = $('.filter__category-checkbox[data-checkboxBody="' + elemParentID + '"]'),
+        checkboxBtnArr = checkboxParent.find('input[type="checkbox"]');
+
+      if(elem.closest('.filter__category-main')) {
+        if($(elem).is(':checked'))  {
+          for(let el of checkboxBtnArr) {
+            if($(el).is(':disabled')) {
+              $(el).closest('.c-form__field').removeClass('is-disabled');
+              $(el)
+                .prop('disabled', false).change()
+                .prop('checked', true).change();
+            }
+          }
+        } else {
+          for(let el of checkboxBtnArr) {
+            $(el).closest('.c-form__field').addClass('is-disabled');
+            $(el)
+              .prop('disabled', true).change()
+              .prop('checked', false).change();
+          }
+        }
       }
-    });
+    })
   };
 	/*
 	* CALLBACK :: end
@@ -279,6 +323,7 @@ $(document).ready((ev) => {
     initSearchExpand();
     initImageCompare();
     initGraph();
+    initFilterRadioBtn();
 
     $('body').on('click', function (e) {
       const className = ".header__search";
